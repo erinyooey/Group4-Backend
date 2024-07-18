0cdb694ce70c387bfd04c8183a414c4939fe6763
Erin Yoo <yooeh@masters.edu>
2024-07-18 08:28:55 -0700

    removed extra index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const { app } = require("../Group4-Backend/src/shared/shared");
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`I am listening on port number ${PORT}`);
-});
-
-const userRoutes = require("../Group4-Backend/src/shared/routes/userRoutes");
-app.use("/api/user", userRoutes);
